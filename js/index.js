746a8d113b5d3074b658eea76c1651fcea89d6f9
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-14 13:55:09 -0600

    WIP finished first couple tasks

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+let logo = document.querySelector('.logo-heading');
+let body = document.querySelector('body');
+
+
+logo.addEventListener('mouseover', function(event){
+    alert('you moused over my logo!')
+})
+
+body.addEventListener('keydown', function(){
+    body.style.backgroundColor = 'orange';
+})
+
+body.addEventListener('keyup', function(){
+    body.style.backgroundColor = 'white';
+})
+
+body.addEventListener('wheel', function(){
+    logo.classList.toggle('.hidden');
+})
